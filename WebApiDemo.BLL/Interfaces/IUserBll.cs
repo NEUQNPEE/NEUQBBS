7a4cf7bffb24b9b40c7caf24a87f052163c11cf0
niefire <103409346+eekspeed@users.noreply.github.com>
2024-08-07 20:13:42 +0800

    引入doxygen(本仓库不包含生成的文档）

--- WebApiDemo.BLL/Interfaces/IUserBll.cs
+++ WebApiDemo.BLL/Interfaces/IUserBll.cs
@@ -3,36 +3,115 @@ using WebApiDemo.Entities.BModels;
 
 namespace WebApiDemo.BLL.Interfaces
 {
+    /// <summary>
+    /// 用户相关业务逻辑的接口
+    /// </summary>
     public interface IUserBll
     {
+        /// <summary>
+        /// debug 获取所有用户
+        /// </summary>
+        /// <returns></returns>
         List<UserBModel>? DebugGetAllUsers();
 
+        /// <summary>
+        /// debug 获取用户
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         UserBModel? DebugGetUserById(int id);
 
+        /// <summary>
+        /// 获取用户
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         UserBModel? GetUserById(int id);
-        UserBModel? GetUserByIdForPost(int id);
+
+        /// <summary>
+        /// 获取用户
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
 
         string GetUserNameById(int id);
 
+        /// <summary>
+        /// 根据用户ID列表获取用户名列表
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <returns></returns>
         List<string> GetUserNamesByIds(List<int> ids);
-        
-        // 查询是否存在该用户名
-        bool CheckUserExist(string userName);
 
-        int CheckLogin(string userName, string password);
+        /// <summary>
+        /// 根据用户ID获取用户用于POST的数据，只保留userName,RigisterTime和Points字段，注意排除IsDeleted字段
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        UserBModel? GetUserByIdForPost(int id);
 
-        string GenerateAutoLoginToken(int userId);
-
-        int CheckAutoLoginToken(string token);
-
-        int AddUser(UserBModel userBModel);
-
-        string UpdateUser(UserBModel userBModel);
-
-        string RemoveUser(int id);
-
+        /// <summary>
+        /// 根据用户ID获取用户基本信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         UserBModel? GetUserBaseInfoById(int id);
 
+        /// <summary>
+        /// 根据用户ID获取用户详细信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         UserBModel? GetUserDetailInfoById(int id);
+
+        /// <summary>
+        /// 检查用户是否存在
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
+        bool CheckUserExist(string userName);
+
+        /// <summary>
+        /// 检查登录
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        int CheckLogin(string userName, string password);
+
+        /// <summary>
+        /// 生成自动登录token
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        string GenerateAutoLoginToken(int userId);
+
+        /// <summary>
+        /// 检查自动登录token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        int CheckAutoLoginToken(string token);
+
+        /// <summary>
+        /// 添加用户
+        /// </summary>
+        /// <param name="userBModel"></param>
+        /// <returns></returns>
+        int AddUser(UserBModel userBModel);
+
+        /// <summary>
+        /// 更新用户
+        /// </summary>
+        /// <param name="userBModel"></param>
+        /// <returns></returns>
+        string UpdateUser(UserBModel userBModel);
+
+        /// <summary>
+        /// 删除用户
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        string RemoveUser(int id);
     }
 }
--- WebApiDemo.BLL/UserBll.cs
+++ WebApiDemo.BLL/UserBll.cs
@@ -5,25 +5,45 @@ using WebApiDemoCommon;
 
 namespace WebApiDemo.BLL
 {
+    /// <summary>
+    /// 用户相关业务逻辑的实现
+    /// </summary>
     public class UserBll : IUserBll
     {
-        // readonly UserDal userDal = new();
-
+        /// <summary>
+        /// debug 获取所有用户
+        /// </summary>
+        /// <returns></returns>
         public List<UserBModel>? DebugGetAllUsers()
         {
             return UserDal.DebugGetAllUsers();
         }
 
+        /// <summary>
+        /// debug 获取用户
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public UserBModel? DebugGetUserById(int id)
         {
             return UserDal.DebugGetUserById(id);
         }
 
+        /// <summary>
+        /// 获取用户
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public UserBModel? GetUserById(int id)
         {
             return UserDal.GetUserById(id);
         }
 
+        /// <summary>
+        /// 根据用户ID获取用户名
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public string GetUserNameById(int id)
         {
             UserBModel? user = UserDal.GetUserById(id);
@@ -35,6 +55,11 @@ namespace WebApiDemo.BLL
             return user.UserName ?? "用户名空引用！";
         }
 
+        /// <summary>
+        /// 根据用户ID列表获取用户名列表
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <returns></returns>
         public List<string> GetUserNamesByIds(List<int> ids)
         {
             List<string> userNames = new();
@@ -46,9 +71,13 @@ namespace WebApiDemo.BLL
             return userNames;
         }
 
+        /// <summary>
+        /// 根据用户ID获取用户用于POST的数据，只保留userName,RigisterTime和Points字段，注意排除IsDeleted字段
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public UserBModel? GetUserByIdForPost(int id)
         {
-            // 只保留userName,RigisterTime和Points字段,注意排除IsDeleted字段
             UserBModel? user = UserDal.GetUserById(id);
             if (user == null)
             {
@@ -63,6 +92,11 @@ namespace WebApiDemo.BLL
             };
         }
 
+        /// <summary>
+        /// 根据用户ID获取用户基本信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public UserBModel? GetUserBaseInfoById(int id)
         {
             UserBModel? user = UserDal.GetUserById(id);
@@ -80,20 +114,36 @@ namespace WebApiDemo.BLL
             };
         }
 
+        /// <summary>
+        /// 根据用户ID获取用户详细信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public UserBModel? GetUserDetailInfoById(int id)
         {
             return UserDal.GetUserById(id);
         }
 
+        /// <summary>
+        /// 检查用户是否存在
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
         public bool CheckUserExist(string userName)
         {
             return UserDal.CheckUserExist(userName);
         }
 
+        /// <summary>
+        /// 检查登录
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
         public int CheckLogin(string userName, string password)
         {
             password = password.ToMd5();
-            List<UserBModel?>? userList = UserDal.GetUserByUserNameAndPassword(userName, password);
+            List<UserBModel>? userList = UserDal.GetUserByUserNameAndPassword(userName, password);
             if (userList?.Count <= 0 || userList == null)
             {
                 return -1;
@@ -101,7 +151,6 @@ namespace WebApiDemo.BLL
 
             // 查出非删除的用户
             UserBModel? user = userList.Find(user => user?.IsDeleted == false);
-            // return user?.Id ?? -1;
             if (user == null)
             {
                 return -1;
@@ -111,11 +160,21 @@ namespace WebApiDemo.BLL
             return user.Id;
         }
 
+        /// <summary>
+        /// 生成自动登录token
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         public string GenerateAutoLoginToken(int userId)
         {
             return UserDal.GenerateAutoLoginToken(userId);
         }
 
+        /// <summary>
+        /// 检查自动登录token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
         public int CheckAutoLoginToken(string token)
         {
             int userId = UserDal.CheckAutoLoginToken(token);
@@ -126,6 +185,11 @@ namespace WebApiDemo.BLL
             return userId;
         }
 
+        /// <summary>
+        /// 添加用户
+        /// </summary>
+        /// <param name="userBModel"></param>
+        /// <returns></returns>
         public int AddUser(UserBModel userBModel)
         {
             if (userBModel.Password == null)
@@ -137,6 +201,11 @@ namespace WebApiDemo.BLL
             return UserDal.AddUser(userBModel);
         }
 
+        /// <summary>
+        /// 更新用户
+        /// </summary>
+        /// <param name="userBModel"></param>
+        /// <returns></returns>
         public string UpdateUser(UserBModel userBModel)
         {
             if (userBModel.Password != null)
@@ -146,6 +215,11 @@ namespace WebApiDemo.BLL
             return UserDal.UpdateUser(userBModel) > 0 ? "更新成功" : "更新失败";
         }
 
+        /// <summary>
+        /// 删除用户
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public string RemoveUser(int id)
         {
             return UserDal.RemoveUser(id) > 0 ? "删除成功" : "删除失败";
